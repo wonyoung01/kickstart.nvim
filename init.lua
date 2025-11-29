db5316db9abb38d9f9cab515fc08fcf1319cfcd5
wonyoung01 <wylee@kaist.ac.kr>
2025-11-29 19:58:03 +0900

    add: shell script formatter

--- init.lua
+++ init.lua
@@ -718,6 +718,9 @@ require('lazy').setup({
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
         'stylua', -- Used to format Lua code
+        'ruff', -- Used to lint and format Python code
+        'shfmt', -- Used to format shell scripts
+        'prettierd', -- Used to format various web development files (markdown, javascript, typescript, etc.)
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
@@ -779,6 +782,7 @@ require('lazy').setup({
         -- markdown = { 'mdformat' },
         markdown = { 'prettierd' },
         latex = { 'latexindent' },
+        sh = { 'shfmt' },
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
         -- javascript = { "prettierd", "prettier", stop_after_first = true },

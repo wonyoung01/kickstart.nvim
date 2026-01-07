9169f036bcf7f57cb50271d5bf323e55d6b54d3f
Wonyoung Lee <wylee@kaist.ac.kr>
2026-01-07 16:01:48 +0900

    add: font

--- lua/custom/plugins/markdown.lua
+++ lua/custom/plugins/markdown.lua
@@ -22,7 +22,7 @@ return {
     'MeanderingProgrammer/render-markdown.nvim',
     dependencies = {
       'nvim-treesitter/nvim-treesitter',
-      'nvim-mini/mini.nvim',
+      'nvim-tree/nvim-web-devicons',
     },
     ft = { 'markdown' },
     opts = {},

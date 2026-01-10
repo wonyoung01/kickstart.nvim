b32e27cf2bf9645eeecaab821012476ea3858622
Wonyoung Lee <wylee@kaist.ac.kr>
2026-01-11 03:13:06 +0900

    add: treesitter setting, currently sticking to master branch

--- init.lua
+++ init.lua
@@ -987,6 +987,8 @@ require('lazy').setup({
   },
   { -- Highlight, edit, and navigate code
     'nvim-treesitter/nvim-treesitter',
+    branch = 'master',
+    lazy = false,
     build = ':TSUpdate',
     main = 'nvim-treesitter.configs', -- Sets main module to use for opts
     -- [[ Configure Treesitter ]] See `:help nvim-treesitter`

e817425eb6d2446c8e824647b1885275258cdccd
Wonyoung Lee <wylee@kaist.ac.kr>
2026-01-31 14:01:21 +0900

    add: term mapping

--- lua/custom/plugins/toggleterm.lua
+++ lua/custom/plugins/toggleterm.lua
@@ -42,6 +42,25 @@ return {
   config = function(_, opts)
     require('toggleterm').setup(opts)
 
+    --------------------------------------------------------------------
+    -- ✅ Terminal Keymaps (your requested mappings)
+    --------------------------------------------------------------------
+    function _G.set_terminal_keymaps()
+      local opts = { buffer = 0 }
+
+      -- window navigation inside terminal
+      vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
+      vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
+      vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)
+      vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]], opts)
+
+      -- allow <C-w> commands
+      vim.keymap.set('t', '<C-w>', [[<C-\><C-n><C-w>]], opts)
+    end
+
+    -- Automatically apply mappings when entering any terminal
+    vim.cmd [[autocmd! TermOpen term://* lua set_terminal_keymaps()]]
+
     -- Optional: If you want autochdir behavior, it's a Vim option (not ToggleTerm)
     -- vim.o.autochdir = true
 
